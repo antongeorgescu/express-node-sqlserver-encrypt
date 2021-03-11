2872e10141009524e74af8e8230a63d57b9f945f
Anton Georgesco <antongeorgescu@gmail.com>
2021-03-11 18:08:52 -0500

    create a local mock active directory

--- routes/authn.js
+++ routes/authn.js
@@ -1,6 +1,18 @@
-var authenticated_permissions = {permissions : [
+const authn_permissions = {permissions : [
     {permission: 'RestrictedAccess', description: 'read restricted to non-sensitive information', password: 'reporter@1'},
     {permission: 'FullAccess', description: 'read sensitive information', password: 'contributor@1'}
 ]};
-module.exports = authenticated_permissions
+
+
+const user_directory = {users : [
+    {user : 'Anton', group : '', role : '', org: '', tenant : '', permission : ''},
+    {user : 'Daniela', group : '', role : '', org: '', tenant : '', permission : ''},
+    {user : 'Alex', group : '', role : '', org: '', tenant : '', permission : ''},
+    {user : 'Kora', group : '', role : '', org: '', tenant : '', permission : ''},
+    {user : 'Jake', group : '', role : '', org: '', tenant : '', permission : ''},
+    {user : 'Tangie', group : '', role : '', org: '', tenant : '', permission : ''},
+]}
+
+
+module.exports = {authn_permissions, user_directory}
 
